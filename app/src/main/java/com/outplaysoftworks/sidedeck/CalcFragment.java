650d6e256a107fb7430448db2fa382d4a30d7d97
wsmithsrs <wsmithsrs@gmailcom>
2016-03-01 18:59:56 -0800

    Initial commit

---
+++ app/src/main/java/com/outplaysoftworks/sidedeck/CalcFragment.java
@@ -0,0 +1,114 @@
+package com.outplaysoftworks.sidedeck;
+
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CalcFragment extends Fragment {
+
+    static TextView numberHolder;
+    static TextView playerOneLP;
+    static TextView playerTwoLP;
+    static Button turnButton;
+
+    public static Integer defaultLP = 8000;
+    static Integer turnNumber = 0;
+    static Integer currentLP1 = defaultLP;
+    static Integer previousLP1;
+    static Integer currentLP2 = defaultLP;
+    static Integer previousLP2;
+
+    public CalcFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_calc, container, false);
+        //Assign view variables after inflation is done
+        numberHolder = (TextView)view.findViewById(R.id.numberHolder);
+        playerOneLP = (TextView)view.findViewById(R.id.playerOneLP);
+        playerOneLP.setText(defaultLP.toString());
+        playerTwoLP = (TextView)view.findViewById(R.id.playerTwoLP);
+        playerTwoLP.setText(defaultLP.toString());
+        turnButton = (Button)view.findViewById(R.id.buttonTurn);
+
+        //return that view
+        return view;
+    }
+
+    public static void setNumberHolderText(String value){
+        numberHolder.setText(value);
+    }
+
+
+    //Animates the transition between numbers when calculation is performed
+    public static void animateTextView(int initialValue, int finalValue, final TextView textview) {
+        if(initialValue != finalValue) { //will not do anything if both values are equal
+            ValueAnimator valueAnimator = ValueAnimator.ofInt((int) initialValue, (int) finalValue);
+            valueAnimator.setDuration(1000);
+
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+                    textview.setText(valueAnimator.getAnimatedValue().toString());
+
+                }
+            });
+            valueAnimator.start();
+        }
+
+    }
+    //Changes LP
+    public static void modLP(String tag, int value){
+        previousLP1 = currentLP1;
+        previousLP2 = currentLP2;
+        switch (tag){
+            case "1+":
+                if(currentLP1 + value < 999999)
+                    currentLP1 += value;
+                else
+                    currentLP1 = 999999;
+                break;
+            case "1-":
+                if(currentLP1 - value > 0)
+                currentLP1 -= value;
+                else
+                    currentLP1 = 0;
+                break;
+            case "2+":
+                if(currentLP2 + value < 999999)
+                    currentLP2 += value;
+                else
+                    currentLP2 = 999999;
+                break;
+            case "2-":
+                if(currentLP2 - value > 0)
+                    currentLP2 -= value;
+                else
+                    currentLP2 = 0;
+                break;
+        }
+        animateTextView(previousLP1, currentLP1, playerOneLP);
+        animateTextView(previousLP2, currentLP2, playerTwoLP);
+    }
+
+}
---
+++ app/src/main/java/com/outplaysoftworks/sidedeck/MainActivity.java
@@ -0,0 +1,219 @@
+package com.outplaysoftworks.sidedeck;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private String numberHolderString = "";
+    private Integer numberHolderNumber = 0;
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Pure layout stuff
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //End of pure layout stuff
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            //return PlaceholderFragment.newInstance(position + 1);
+            switch(position){
+                case 0: return new CalcFragment();
+                case 1: return new LogFragment();
+            }
+            return new LogFragment();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Calculator";
+                case 1:
+                    return "Duel Log";
+            }
+            return null;
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+/*
+        Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+*/
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        CharSequence mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    //Appends whatever numerical button is pressed to the lp calc preview
+    public void addToNumberHolder(View view){
+        if(numberHolderString.length()<5){
+            numberHolderString += view.getTag();
+            numberHolderNumber = Integer.parseInt(numberHolderString);
+            CalcFragment.setNumberHolderText(numberHolderString);
+        }
+
+        if(numberHolderString.length() > 5){
+            numberHolderString = "99999";
+            numberHolderNumber = 99999;
+            CalcFragment.setNumberHolderText(numberHolderString);
+        }
+        System.out.println(numberHolderNumber + ", string: " + numberHolderString);
+    }
+
+    //Sets everything related to lp calc preview to 0 or empty
+    public void resetNumberHolder(View view){
+        numberHolderNumber = 0;
+        numberHolderString = "";
+        CalcFragment.setNumberHolderText(numberHolderString);
+    }
+
+    public void modLP(View view){
+        CalcFragment.modLP(view.getTag().toString(), numberHolderNumber);
+        numberHolderNumber = 0;
+        numberHolderString = "";
+        CalcFragment.setNumberHolderText(numberHolderString);
+    }
+}
